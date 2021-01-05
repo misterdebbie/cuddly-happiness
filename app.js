a6b4fb7cb0abdca81d5e54591d54d82fb5b320dc
misterdebbie <giyoungchong1980@gmail.com>
2021-01-04 18:51:32 -0600

    Project setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) => {
+  res.render('main');
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});

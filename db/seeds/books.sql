da8236b3621f4fa9b2cf18d0cc3d3c6a856b795e
misterdebbie <giyoungchong1980@gmail.com>
2021-01-04 19:48:11 -0600

    Run migration for db

---
+++ db/seeds/books.sql
@@ -0,0 +1,17 @@
+INSERT INTO books (title, description) VALUES
+  (
+    'The Floating Opera',
+    'A comic account of life'
+  ),
+  (
+    'Infinite Jest',
+    'How humans consume content'
+  ),
+  (
+    'The Tin Drum',
+    'Ahhhhhhhh'
+  ),
+  (
+    'The Witches',
+    'A boy as mouse'
+  );
